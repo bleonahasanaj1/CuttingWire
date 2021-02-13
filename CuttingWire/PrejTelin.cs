96fa347454a166fe97e9d7c4eb0124640d18d2fb
bleonahasanaj1 <75685284+bleonahasanaj1@users.noreply.github.com>
2021-02-13 21:07:23 +0100

    Cutting wire, find optimal cutting combination for maximal profit

---
+++ CuttingWire/PrejTelin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CuttingWire
+{
+    class PrejTelin
+    {
+        public int prej(int[] cmimet, int gjatesia)
+        {
+            int[] rezultati = new int[gjatesia + 1];
+            rezultati[0] = 0;
+
+            for (int i = 1; i <= gjatesia; i++)
+            {
+                int max_val = int.MinValue;
+                for (int j = 0; j < i; j++)
+                {
+                    max_val = Math.Max(max_val, cmimet[j] + rezultati[i - j - 1]);
+                } 
+                rezultati[i] = max_val;
+            }
+            return rezultati[gjatesia];
+        }
+    }
+}
